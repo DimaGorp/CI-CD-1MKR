66de4bcc5ec43bbecfc4549130ac6c127250e266
maks667 <dgorpinuk50@gmail.com>
2024-04-03 09:31:13 +0300

    added split_text function

--- tasks.py
+++ tasks.py
@@ -1,13 +1,23 @@
 import re
+
 def read_file(filename):
     """Функція для зчитування вмісту файлу і повернення тексту."""
     with open(filename, 'r') as file:
         return file.read()
 
+def split_text(text):
+    """Функція для розділення тексту на слова та речення."""
+    words = re.findall(r'\b\w+\b', text)
+    sentences = re.split(r'[.!?]+', text)
+    sentences = [sentence for sentence in sentences if sentence.strip()]
+    return words, sentences
+
 def main():
     filename = "index.txt"  # Шлях до файлу
     text = read_file(filename)
-    print(text)
+    words, sentences = split_text(text)
+    print("Слова:", words)
+    print("Речення:", sentences)
 
 if __name__ == "__main__":
     main()
