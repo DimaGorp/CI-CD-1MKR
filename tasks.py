efa8e76f4b8c38db871143714662fc064f239083
maks667 <dgorpinuk50@gmail.com>
2024-04-03 09:32:50 +0300

    added count_words_and_sentences function

--- tasks.py
+++ tasks.py
@@ -12,12 +12,17 @@ def split_text(text):
     sentences = [sentence for sentence in sentences if sentence.strip()]
     return words, sentences
 
+def count_words_and_sentences(words, sentences):
+    """Функція для підрахунку кількості слів та речень."""
+    return len(words), len(sentences)
+
 def main():
     filename = "index.txt"  # Шлях до файлу
     text = read_file(filename)
     words, sentences = split_text(text)
-    print("Слова:", words)
-    print("Речення:", sentences)
+    word_count, sentence_count = count_words_and_sentences(words, sentences)
+    print("Кількість слів у файлі:", word_count)
+    print("Кількість речень у файлі:", sentence_count)
 
 if __name__ == "__main__":
     main()
