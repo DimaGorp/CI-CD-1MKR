a82d1d8f66e488f15bcc6b43c902367ba1e2b28a
maks667 <dgorpinuk50@gmail.com>
2024-04-03 09:39:01 +0300

    updated tasks.py, fixed bugs in split_text function

--- tasks.py
+++ tasks.py
@@ -1,18 +1,30 @@
 import re
+from typing import List, Tuple
 
-def read_file(filename):
+def read_file(filename: str) -> str:
     """Функція для зчитування вмісту файлу і повернення тексту."""
     with open(filename, 'r') as file:
         return file.read()
 
-def split_text(text):
+def split_text(text: str) -> Tuple[List[str], List[str]]:
     """Функція для розділення тексту на слова та речення."""
     words = re.findall(r'\b\w+\b', text)
+    
+    # Розділяємо текст на речення за всіма можливими символами, які можуть завершувати речення
     sentences = re.split(r'[.!?]+', text)
-    sentences = [sentence for sentence in sentences if sentence.strip()]
-    return words, sentences
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
 
-def count_words_and_sentences(words, sentences):
+    # Розділяємо кожне речення на слова, враховуючи розділові знаки
+    final_sentences = []
+    separators = ",:; "
+    for sentence in sentences:
+        for separator in separators:
+            sentence = sentence.replace(separator, "")
+        final_sentences.extend(sentence.split())
+    
+    return words, final_sentences
+
+def count_words_and_sentences(words: List[str], sentences: List[str]) -> Tuple[int, int]:
     """Функція для підрахунку кількості слів та речень."""
     return len(words), len(sentences)
 
